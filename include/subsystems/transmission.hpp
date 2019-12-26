91122404e807a7cacffb41e59fe2ef6bcee2afcf
ZachDaChampion <zachchampion79@gmail.com>
2019-12-25 19:40:36 -0500

    possibly finish transmission header

--- include/subsystems/transmission.hpp
+++ include/subsystems/transmission.hpp
@@ -3,30 +3,77 @@
 namespace transmission {
 
 
-  // behavior states that the transmission may be in
+  /**
+   * Describe a way for the transmission to reconcile the chassis and tilter.
+   */
   enum class Behavior {
-    PASSIVE,          // transmission motor copies dedicated motor; tilter should remain stationary but this is not enforced
-    HOLDING,          // tilter is held in place; drive speed may be modified
-    RETRACTING,       // transmission motor is locked at full reverse speed regardless of what the direct motor does
-    EXTENDING_FAST,   // transmission motor is locked at full forward speed regardless of what the direct motor does
-    EXTENDING_SLOW    // transmission motor slowly extends with a controlled velocity and acceleration; drive speed may be modified
+    PASSIVE,          ///< transmission motor copies dedicated motor; tilter should remain stationary but this is not enforced
+    HOLDING,          ///< tilter is held in place; drive speed may be modified
+    RETRACTING,       ///< transmission motor is locked at full reverse speed regardless of what the direct motor does
+    EXTENDING_FAST,   ///< transmission motor is locked at full forward speed regardless of what the direct motor does
+    EXTENDING_SLOW    ///< transmission motor slowly extends with a controlled velocity and acceleration; drive speed may be modified
   };
 
 
-  // states that the transmission may be in
+  /**
+   * Describe the sates that the transmission may be in.
+   */
   enum class State {
-    LOCKED,             // fully retracted and locked (PASSIVE behavior)
-    WAITING_FOR_LOCK,   // fully retracted but not locked (HOLDING behavior)
-    RETRACTING,         // retracting (RETRACTING behavior)
-    DEPOSITING_FAST,    // first stage of deposit, at full speed (EXTENDING_FAST behavior)
-    DEPOSITING_DECEL,   // second stage of deposit, decelerating to avoid flinging cubes (EXTENDING_SLOW behavior)
-    DEPOSITED           // fully deposited but still extended (HOLDING behavior)
+    LOCKED,             ///< fully retracted and locked (PASSIVE behavior)
+    WAITING_FOR_UNLOCK, ///< fully retracted and locked but desiring to be extended (PASSIVE behavior)
+    WAITING_FOR_LOCK,   ///< fully retracted but not locked (HOLDING behavior)
+    RETRACTING,         ///< retracting (RETRACTING behavior)
+    DEPOSITING_FAST,    ///< first stage of deposit, at full speed (EXTENDING_FAST behavior)
+    DEPOSITING_DECEL,   ///< second stage of deposit, decelerating to avoid flinging cubes (EXTENDING_SLOW behavior)
+    DEPOSITED           ///< fully deposited but still extended (HOLDING behavior)
   };
 
 
-  // get the current behavior of the transmission
+  /**
+   * Get the current behavior of the transmission.
+   * 
+   * @return current behavior of the transmission
+   */
   Behavior get_behavior();
 
-  // get the current state of the transmission
+
+  /**
+   * Get the current state of the transmission.
+   * 
+   * @return current state of the transmission
+   */
   State get_state();
+
+
+  /**
+   * Set the desired voltage of each side of the chassis.
+   * Actual voltage is determined by the current behavior.
+   *
+   * @param left   desired voltage of left side
+   * @param right  desired voltage of left side
+   */
+  void move_voltage(int left, int right);
+
+
+  /**
+   * Set the desired voltage of both sides of the chassis.
+   * Actual voltage is determined by the current behavior.
+   *
+   * @param val  desired voltage of both sides
+   */
+  void move_voltage(int val);
+
+
+  /**
+   * Begin stack depositing.
+   * State will end at DEPOSITED.
+   */
+   void deposit_stack();
+
+
+    /**
+     * Begin to retract the tray.
+     * State will end at LOCKED.
+     */
+   void retract_tray();
 }
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -3,7 +3,6 @@
 void opcontrol() {
   while(true) {
 
-
     pros::delay(10);
   }
 }
